d1f115c4c5000c911a6c6a45fd2c362baf010132
Muhammad Alkhateeb <mhammadalkhateeb22@gmail.com>
2024-05-03 20:20:14 +0300

    identifying the affected area

--- X-Ray Images Project/Form1.Designer.cs
+++ X-Ray Images Project/Form1.Designer.cs
@@ -1,16 +1,162 @@
-﻿namespace X_Ray_Images_Project
+﻿//namespace X_Ray_Images_Project
+//{
+//    partial class Form1
+//    {
+//        /// <summary>
+//        ///  Required designer variable.
+//        /// </summary>
+//        private System.ComponentModel.IContainer components = null;
+
+//        /// <summary>
+//        ///  Clean up any resources being used.
+//        /// </summary>
+//        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+//        protected override void Dispose(bool disposing)
+//        {
+//            if (disposing && (components != null))
+//            {
+//                components.Dispose();
+//            }
+//            base.Dispose(disposing);
+//        }
+
+//        #region Windows Form Designer generated code
+
+//        /// <summary>
+//        ///  Required method for Designer support - do not modify
+//        ///  the contents of this method with the code editor.
+//        /// </summary>
+//        private void InitializeComponent()
+//        {
+//            button1 = new Button();
+//            inputImage = new PictureBox();
+//            ((System.ComponentModel.ISupportInitialize)inputImage).BeginInit();
+//            SuspendLayout();
+//            // 
+//            // button1
+//            // 
+//            button1.Location = new Point(254, 234);
+//            button1.Name = "button1";
+//            button1.Size = new Size(136, 33);
+//            button1.TabIndex = 0;
+//            button1.Text = "upload image";
+//            button1.UseVisualStyleBackColor = true;
+//            button1.Click += button1_Click;
+//            // 
+//            // inputImage
+//            // 
+//            inputImage.BackColor = SystemColors.ButtonFace;
+//            inputImage.BorderStyle = BorderStyle.FixedSingle;
+//            inputImage.Location = new Point(220, 75);
+//            inputImage.Name = "inputImage";
+//            inputImage.Size = new Size(216, 132);
+//            inputImage.SizeMode = PictureBoxSizeMode.Zoom;
+//            inputImage.TabIndex = 1;
+//            inputImage.TabStop = false;
+//            // 
+//            // Form1
+//            // 
+//            AutoScaleDimensions = new SizeF(7F, 15F);
+//            AutoScaleMode = AutoScaleMode.Font;
+//            BackColor = SystemColors.ActiveCaption;
+//            ClientSize = new Size(661, 421);
+//            Controls.Add(inputImage);
+//            Controls.Add(button1);
+//            Name = "Form1";
+//            Text = "X-Ray Images";
+//            ((System.ComponentModel.ISupportInitialize)inputImage).EndInit();
+//            ResumeLayout(false);
+//        }
+
+//        #endregion
+
+//        private Button button1;
+//        private PictureBox inputImage;
+//    }
+//}
+//namespace X_Ray_Images_Project
+//{
+//    partial class Form1
+//    {
+//        private System.ComponentModel.IContainer components = null;
+
+//        protected override void Dispose(bool disposing)
+//        {
+//            if (disposing && (components != null))
+//            {
+//                components.Dispose();
+//            }
+//            base.Dispose(disposing);
+//        }
+
+//        private void InitializeComponent()
+//        {
+//            this.btnIdentifyAffectedAreas = new System.Windows.Forms.Button();
+//            this.button1 = new System.Windows.Forms.Button();
+//            this.inputImage = new System.Windows.Forms.PictureBox();
+//            ((System.ComponentModel.ISupportInitialize)(this.inputImage)).BeginInit();
+//            this.SuspendLayout();
+//            // 
+//            // btnIdentifyAffectedAreas
+//            // 
+//            this.btnIdentifyAffectedAreas.Location = new System.Drawing.Point(12, 12);
+//            this.btnIdentifyAffectedAreas.Name = "btnIdentifyAffectedAreas";
+//            this.btnIdentifyAffectedAreas.Size = new System.Drawing.Size(170, 23);
+//            this.btnIdentifyAffectedAreas.TabIndex = 0;
+//            this.btnIdentifyAffectedAreas.Text = "Identify Affected Areas";
+//            this.btnIdentifyAffectedAreas.UseVisualStyleBackColor = true;
+//            this.btnIdentifyAffectedAreas.Click += new System.EventHandler(this.btnIdentifyAffectedAreas_Click);
+//            // 
+//            // button1
+//            // 
+//            this.button1.Location = new System.Drawing.Point(188, 12);
+//            this.button1.Name = "button1";
+//            this.button1.Size = new System.Drawing.Size(170, 23);
+//            this.button1.TabIndex = 1;
+//            this.button1.Text = "Open Radiograph";
+//            this.button1.UseVisualStyleBackColor = true;
+//            this.button1.Click += new System.EventHandler(this.button1_Click);
+//            // 
+//            // inputImage
+//            // 
+//            this.inputImage.Location = new System.Drawing.Point(12, 41);
+//            this.inputImage.Name = "inputImage";
+//            this.inputImage.Size = new System.Drawing.Size(346, 307);
+//            this.inputImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+//            this.inputImage.TabIndex = 2;
+//            this.inputImage.TabStop = false;
+//            this.inputImage.MouseDown += new System.Windows.Forms.MouseEventHandler(this.inputImage_MouseDown);
+//            this.inputImage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.inputImage_MouseMove);
+//            this.inputImage.MouseUp += new System.Windows.Forms.MouseEventHandler(this.inputImage_MouseUp);
+//            this.inputImage.Paint += new System.Windows.Forms.PaintEventHandler(this.inputImage_Paint);
+//            // 
+//            // Form1
+//            // 
+//            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+//            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+//            this.ClientSize = new System.Drawing.Size(370, 360);
+//            this.Controls.Add(this.inputImage);
+//            this.Controls.Add(this.button1);
+//            this.Controls.Add(this.btnIdentifyAffectedAreas);
+//            this.Name = "Form1";
+//            this.Text = "X-Ray Images Project";
+//            ((System.ComponentModel.ISupportInitialize)(this.inputImage)).EndInit();
+//            this.ResumeLayout(false);
+
+//        }
+
+//        private System.Windows.Forms.Button btnIdentifyAffectedAreas;
+//        private System.Windows.Forms.Button button1;
+//        private System.Windows.Forms.PictureBox inputImage;
+//    }
+//}
+
+namespace X_Ray_Images_Project
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,57 +166,67 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
+            btnIdentifyAffectedAreas = new Button();
             button1 = new Button();
             inputImage = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)inputImage).BeginInit();
             SuspendLayout();
             // 
+            // btnIdentifyAffectedAreas
+            // 
+            btnIdentifyAffectedAreas.Location = new Point(14, 14);
+            btnIdentifyAffectedAreas.Margin = new Padding(4, 3, 4, 3);
+            btnIdentifyAffectedAreas.Name = "btnIdentifyAffectedAreas";
+            btnIdentifyAffectedAreas.Size = new Size(198, 27);
+            btnIdentifyAffectedAreas.TabIndex = 0;
+            btnIdentifyAffectedAreas.Text = "Identify Affected Areas";
+            btnIdentifyAffectedAreas.UseVisualStyleBackColor = true;
+            // 
             // button1
             // 
-            button1.Location = new Point(254, 234);
+            button1.Location = new Point(219, 14);
+            button1.Margin = new Padding(4, 3, 4, 3);
             button1.Name = "button1";
-            button1.Size = new Size(136, 33);
-            button1.TabIndex = 0;
-            button1.Text = "upload image";
+            button1.Size = new Size(198, 27);
+            button1.TabIndex = 1;
+            button1.Text = "Open Radiograph";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
             // inputImage
             // 
-            inputImage.BackColor = SystemColors.ButtonFace;
-            inputImage.BorderStyle = BorderStyle.FixedSingle;
-            inputImage.Location = new Point(220, 75);
+            inputImage.Location = new Point(14, 47);
+            inputImage.Margin = new Padding(4, 3, 4, 3);
             inputImage.Name = "inputImage";
-            inputImage.Size = new Size(216, 132);
+            inputImage.Size = new Size(404, 354);
             inputImage.SizeMode = PictureBoxSizeMode.Zoom;
-            inputImage.TabIndex = 1;
+            inputImage.TabIndex = 2;
             inputImage.TabStop = false;
+            inputImage.Click += inputImage_Click;
+            inputImage.Paint += inputImage_Paint;
+            inputImage.MouseDown += inputImage_MouseDown;
+            inputImage.MouseMove += inputImage_MouseMove;
+            inputImage.MouseUp += inputImage_MouseUp;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(661, 421);
+            ClientSize = new Size(432, 415);
             Controls.Add(inputImage);
             Controls.Add(button1);
+            Controls.Add(btnIdentifyAffectedAreas);
+            Margin = new Padding(4, 3, 4, 3);
             Name = "Form1";
-            Text = "X-Ray Images";
+            Text = "X-Ray Images Project";
             ((System.ComponentModel.ISupportInitialize)inputImage).EndInit();
             ResumeLayout(false);
         }
 
-        #endregion
-
-        private Button button1;
-        private PictureBox inputImage;
+        private System.Windows.Forms.Button btnIdentifyAffectedAreas;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox inputImage;
     }
 }
--- X-Ray Images Project/Form1.cs
+++ X-Ray Images Project/Form1.cs
@@ -1,7 +1,162 @@
+//namespace X_Ray_Images_Project
+//{
+//    public partial class Form1 : Form
+//    {
+//        public Form1()
+//        {
+//            InitializeComponent();
+//        }
+
+//        private void button1_Click(object sender, EventArgs e)
+//        {
+//            OpenFileDialog openFileDialog = new OpenFileDialog();
+//            openFileDialog.Filter = "Image Files (*.jpg, *.png)|*.jpg;*.png";
+
+//            if (openFileDialog.ShowDialog() == DialogResult.OK)
+//            {
+//                try
+//                {
+//                    string imageLocation = openFileDialog.FileName;
+//                    inputImage.ImageLocation = imageLocation;
+//                }
+//                catch (Exception ex)
+//                {
+//                    MessageBox.Show($"An error occurred: {ex.Message}");
+//                }
+//            }
+//        }
+//    }
+//}
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+//namespace X_Ray_Images_Project
+//{
+//    public partial class Form1 : Form
+//    {
+//        private Bitmap originalRadiograph; // Store the original radiograph image
+//        private Rectangle selectedArea; // Store the selected area by the mouse
+
+//        public Form1()
+//        {
+//            InitializeComponent();
+//        }
+
+//        private void button1_Click(object sender, EventArgs e)
+//        {
+//            OpenFileDialog openFileDialog = new OpenFileDialog();
+//            openFileDialog.Filter = "Image Files (*.jpg, *.png)|*.jpg;*.png";
+
+//            if (openFileDialog.ShowDialog() == DialogResult.OK)
+//            {
+//                try
+//                {
+//                    string imageLocation = openFileDialog.FileName;
+//                    inputImage.ImageLocation = imageLocation;
+
+//                    // Load the original radiograph
+//                    originalRadiograph = new Bitmap(imageLocation);
+//                }
+//                catch (Exception ex)
+//                {
+//                    MessageBox.Show($"An error occurred: {ex.Message}");
+//                }
+//            }
+//        }
+
+//        private void inputImage_MouseDown(object sender, MouseEventArgs e)
+//        {
+//            // Start selecting the affected area
+//            selectedArea = new Rectangle(e.Location, new Size(0, 0));
+//        }
+
+//        private void inputImage_MouseMove(object sender, MouseEventArgs e)
+//        {
+//            if (e.Button == MouseButtons.Left)
+//            {
+//                // Update the selected area while moving the mouse
+//                selectedArea.Width = e.X - selectedArea.X;
+//                selectedArea.Height = e.Y - selectedArea.Y;
+
+//                inputImage.Refresh(); // Refresh the picture box to draw the rectangle
+//            }
+//        }
+
+//        private void inputImage_MouseUp(object sender, MouseEventArgs e)
+//        {
+//            // Finish selecting the affected area
+//            selectedArea.Width = e.X - selectedArea.X;
+//            selectedArea.Height = e.Y - selectedArea.Y;
+
+//            // Perform affected area identification
+//            Rectangle[] affectedAreas = IdentifyAffectedAreas(selectedArea);
+
+//            // Draw rectangles around the affected areas
+//            using (Graphics graphics = Graphics.FromImage(originalRadiograph))
+//            {
+//                Pen pen = new Pen(Color.Red, 2);
+//                foreach (Rectangle area in affectedAreas)
+//                {
+//                    graphics.DrawRectangle(pen, area);
+//                }
+//            }
+
+//            // Update the picture box with the modified radiograph
+//            inputImage.Image = originalRadiograph;
+//            inputImage.Refresh(); // Refresh the picture box to display the modified radiograph
+//        }
+
+//        private Rectangle[] IdentifyAffectedAreas(Rectangle selectedArea)
+//        {
+//            // Add your implementation here to identify the affected areas within the selectedArea
+//            // using image processing techniques or machine learning algorithms
+
+//            // For demonstration purposes, we'll assume the selectedArea itself is the affected area
+//            return new Rectangle[] { selectedArea };
+//        }
+
+//        private void inputImage_Paint(object sender, PaintEventArgs e)
+//        {
+//            // Draw the selected area rectangle on the picture box
+//            if (selectedArea.Width > 0 && selectedArea.Height > 0)
+//            {
+//                e.Graphics.DrawRectangle(Pens.Blue, selectedArea);
+//            }
+//        }
+
+//        private void btnIdentifyAffectedAreas_Click(object sender, EventArgs e)
+//        {
+//            // Handle the button click event to identify affected areas
+//            // You can add your logic here to perform the identification
+//            // For example, you can call the IdentifyAffectedAreas() method
+//            // and display the results in a message box or any other way you prefer.
+//            MessageBox.Show("Identifying affected areas...");
+//        }
+//    }
+//}
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace X_Ray_Images_Project
 {
     public partial class Form1 : Form
     {
+        private Bitmap originalRadiograph;
+        private Rectangle selectedArea;
+        private bool isSelecting;
+
         public Form1()
         {
             InitializeComponent();
@@ -9,21 +164,68 @@ namespace X_Ray_Images_Project
 
         private void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Image Files (*.jpg, *.png)|*.jpg;*.png";
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image Files (*.jpg, *.png)|*.jpg;*.png"
+            };
 
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 try
                 {
-                    string imageLocation = openFileDialog.FileName;
-                    inputImage.ImageLocation = imageLocation;
+                    originalRadiograph = new Bitmap(openFileDialog.FileName);
+                    inputImage.Image = (Bitmap)originalRadiograph.Clone();
+                    MessageBox.Show("Image loaded successfully.");
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show($"An error occurred: {ex.Message}");
+                    MessageBox.Show($"An error occurred while loading the image: {ex.Message}");
                 }
             }
         }
+
+        private void inputImage_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                selectedArea = new Rectangle(e.Location, new Size());
+                isSelecting = true;
+            }
+        }
+
+        private void inputImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && isSelecting)
+            {
+                selectedArea.Width = e.X - selectedArea.Left;
+                selectedArea.Height = e.Y - selectedArea.Top;
+                inputImage.Invalidate();  // Force the PictureBox to be redrawn
+            }
+        }
+
+        private void inputImage_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (isSelecting)
+            {
+                selectedArea.Width = e.X - selectedArea.Left;
+                selectedArea.Height = e.Y - selectedArea.Top;
+                isSelecting = false;
+                inputImage.Invalidate();  // Force refresh to clear the temporary rectangle
+            }
+        }
+
+        private void inputImage_Paint(object sender, PaintEventArgs e)
+        {
+            if (isSelecting || (selectedArea.Width > 0 && selectedArea.Height > 0))
+            {
+                e.Graphics.DrawRectangle(Pens.Blue, selectedArea);
+            }
+        }
+
+        private void inputImage_Click(object sender, EventArgs e)
+        {
+
+        }   
     }
 }
+
