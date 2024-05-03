601f4643f740572de692ab7d682f18ba9608542e
Muhammad Alkhateeb <mhammadalkhateeb22@gmail.com>
2024-05-03 07:25:19 +0300

    upload image

--- X-Ray Images Project/Form1.Designer.cs
+++ X-Ray Images Project/Form1.Designer.cs
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            inputImage = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)inputImage).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(254, 234);
+            button1.Name = "button1";
+            button1.Size = new Size(136, 33);
+            button1.TabIndex = 0;
+            button1.Text = "upload image";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // inputImage
+            // 
+            inputImage.BackColor = SystemColors.ButtonFace;
+            inputImage.BorderStyle = BorderStyle.FixedSingle;
+            inputImage.Location = new Point(220, 75);
+            inputImage.Name = "inputImage";
+            inputImage.Size = new Size(216, 132);
+            inputImage.SizeMode = PictureBoxSizeMode.Zoom;
+            inputImage.TabIndex = 1;
+            inputImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(661, 421);
+            Controls.Add(inputImage);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "X-Ray Images";
+            ((System.ComponentModel.ISupportInitialize)inputImage).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private PictureBox inputImage;
     }
 }
--- X-Ray Images Project/Form1.cs
+++ X-Ray Images Project/Form1.cs
@@ -6,5 +6,24 @@ namespace X_Ray_Images_Project
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg, *.png)|*.jpg;*.png";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string imageLocation = openFileDialog.FileName;
+                    inputImage.ImageLocation = imageLocation;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred: {ex.Message}");
+                }
+            }
+        }
     }
 }
