33d067b2bb425210ff62a5326aa43bfa2e8efb3b
Muhammad Alkhateeb <mhammadalkhateeb22@gmail.com>
2024-05-17 07:02:12 +0300

    First part done (initially) mh

--- X-Ray Images Project/Form1.Designer.cs
+++ X-Ray Images Project/Form1.Designer.cs
@@ -24,12 +24,15 @@
         {
             button1 = new Button();
             inputImage = new PictureBox();
+            comboColorMap = new ComboBox();
+            label1 = new Label();
+            saveImage = new Button();
             ((System.ComponentModel.ISupportInitialize)inputImage).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(115, 14);
+            button1.Location = new Point(32, 12);
             button1.Margin = new Padding(4, 3, 4, 3);
             button1.Name = "button1";
             button1.Size = new Size(198, 27);
@@ -40,7 +43,7 @@
             // 
             // inputImage
             // 
-            inputImage.Location = new Point(14, 47);
+            inputImage.Location = new Point(14, 94);
             inputImage.Margin = new Padding(4, 3, 4, 3);
             inputImage.Name = "inputImage";
             inputImage.Size = new Size(404, 354);
@@ -51,11 +54,45 @@
             inputImage.MouseMove += inputImage_MouseMove;
             inputImage.MouseUp += inputImage_MouseUp;
             // 
+            // comboColorMap
+            // 
+            comboColorMap.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboColorMap.FormattingEnabled = true;
+            comboColorMap.Items.AddRange(new object[] { "Hot", "Cool", "Jet" });
+            comboColorMap.Location = new Point(115, 53);
+            comboColorMap.SelectedIndex = 0;
+            comboColorMap.Name = "comboColorMap";
+            comboColorMap.Size = new Size(121, 23);
+            comboColorMap.TabIndex = 3;
+            comboColorMap.SelectedIndexChanged += comboColorMap_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Select ColorMap";
+            // 
+            // saveImage
+            // 
+            saveImage.Location = new Point(32, 463);
+            saveImage.Name = "saveImage";
+            saveImage.Size = new Size(198, 29);
+            saveImage.TabIndex = 5;
+            saveImage.Text = "Save Image";
+            saveImage.UseVisualStyleBackColor = true;
+            saveImage.Click += saveImage_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(432, 415);
+            ClientSize = new Size(433, 504);
+            Controls.Add(saveImage);
+            Controls.Add(label1);
+            Controls.Add(comboColorMap);
             Controls.Add(inputImage);
             Controls.Add(button1);
             Margin = new Padding(4, 3, 4, 3);
@@ -63,9 +100,13 @@
             Text = "X-Ray Images Project";
             ((System.ComponentModel.ISupportInitialize)inputImage).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         private Button button1;
-        private PictureBox inputImage;  
+        private PictureBox inputImage;
+        private ComboBox comboColorMap;
+        private Label label1;
+        private Button saveImage;
     }
 }
--- X-Ray Images Project/Form1.cs
+++ X-Ray Images Project/Form1.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Windows.Forms;
 
 namespace X_Ray_Images_Project
@@ -7,6 +9,9 @@ namespace X_Ray_Images_Project
     public partial class Form1 : Form
     {
         private Bitmap originalRadiograph;
+        private Bitmap resizedImage;
+        private List<Rectangle> selectedAreas = new List<Rectangle>();
+        private HashSet<Point> coloredPixels = new HashSet<Point>();
         private Rectangle selectedArea;
         private bool isSelecting;
         private Point startPoint;
@@ -28,13 +33,26 @@ namespace X_Ray_Images_Project
                 try
                 {
                     originalRadiograph = new Bitmap(openFileDialog.FileName);
-                    inputImage.Image = (Bitmap)originalRadiograph.Clone();
+                    resizedImage = ResizeImage(originalRadiograph, inputImage.Width, inputImage.Height);
+                    inputImage.Image = resizedImage;
+                    selectedAreas.Clear();
+                    coloredPixels.Clear();
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show($"An error occurred while loading the image: {ex.Message}");
                 }
             }
+        }
+
+        private Bitmap ResizeImage(Bitmap originalImage, int targetWidth, int targetHeight)
+        {
+            Bitmap resizedImage = new Bitmap(targetWidth, targetHeight);
+            using (Graphics g = Graphics.FromImage(resizedImage))
+            {
+                g.DrawImage(originalImage, 0, 0, targetWidth, targetHeight);
+            }
+            return resizedImage;
         }
 
         private void inputImage_MouseDown(object sender, MouseEventArgs e)
@@ -56,66 +74,136 @@ namespace X_Ray_Images_Project
                 int width = Math.Abs(startPoint.X - e.X);
                 int height = Math.Abs(startPoint.Y - e.Y);
 
-                // Update the selected area
                 selectedArea = new Rectangle(x, y, width, height);
-                inputImage.Invalidate();  // Force the PictureBox to be redrawn
+                inputImage.Invalidate();
             }
         }
-
 
         private void inputImage_MouseUp(object sender, MouseEventArgs e)
         {
             if (isSelecting)
             {
                 isSelecting = false;
-                ApplyColorMapping();  // Apply color mapping to the selected area
-                inputImage.Invalidate();  // Force refresh to clear the temporary rectangle
+                selectedAreas.Add(selectedArea);
+                ApplyColorMapping();
+                inputImage.Invalidate();
             }
         }
 
         private void inputImage_Paint(object sender, PaintEventArgs e)
         {
-            if (isSelecting || (selectedArea.Width > 0 && selectedArea.Height > 0))
+            foreach (var area in selectedAreas)
+            {
+                e.Graphics.DrawRectangle(Pens.Yellow, area);
+            }
+            if (isSelecting)
             {
                 e.Graphics.DrawRectangle(Pens.Yellow, selectedArea);
             }
         }
 
-        private Color MapColor(int grayscaleValue)
+        private Point PictureBoxToImage(Point pt)
         {
-            // Map grayscale value to a color gradient from black to green and white to red
-            int red = grayscaleValue < 128 ? 255 : 255 - (grayscaleValue - 128) * 2;
-            int green = grayscaleValue < 128 ? grayscaleValue * 2 : 255;
-            return Color.FromArgb(red, green, 0);
+            float xRatio = (float)originalRadiograph.Width / inputImage.ClientSize.Width;
+            float yRatio = (float)originalRadiograph.Height / inputImage.ClientSize.Height;
+            return new Point((int)(pt.X * xRatio), (int)(pt.Y * yRatio));
+        }
+
+        private Rectangle PictureBoxToImage(Rectangle rect)
+        {
+            Point topLeft = PictureBoxToImage(rect.Location);
+            Point bottomRight = PictureBoxToImage(new Point(rect.Right, rect.Bottom));
+            return new Rectangle(topLeft, new Size(bottomRight.X - topLeft.X, bottomRight.Y - topLeft.Y));
         }
 
         private void ApplyColorMapping()
         {
             if (originalRadiograph != null)
             {
-                // Ensure the selected area is valid
-                if (selectedArea.Width > 0 && selectedArea.Height > 0)
+                foreach (var area in selectedAreas)
                 {
-                    // Iterate through the selected area and apply color mapping
-                    for (int y = selectedArea.Top; y < selectedArea.Bottom; y++)
+                    Rectangle actualArea = PictureBoxToImage(area);
+                    for (int y = actualArea.Top; y < actualArea.Bottom; y++)
                     {
-                        for (int x = selectedArea.Left; x < selectedArea.Right; x++)
+                        for (int x = actualArea.Left; x < actualArea.Right; x++)
                         {
-                            Color originalColor = originalRadiograph.GetPixel(x, y);
-                            int grayscaleValue = (int)(originalColor.GetBrightness() * 255); // Convert to grayscale value
-                            Color newColor = MapColor(grayscaleValue);
-                            originalRadiograph.SetPixel(x, y, newColor);
+                            Point point = new Point(x, y);
+                            if (!coloredPixels.Contains(point))
+                            {
+                                if (x >= 0 && x < originalRadiograph.Width && y >= 0 && y < originalRadiograph.Height)
+                                {
+                                    Color originalColor = originalRadiograph.GetPixel(x, y);
+                                    int grayscaleValue = (int)(originalColor.GetBrightness() * 255);
+                                    Color newColor = MapColor(grayscaleValue);
+                                    originalRadiograph.SetPixel(x, y, newColor);
+                                    coloredPixels.Add(point);
+                                }
+                            }
                         }
                     }
-                    // Update the PictureBox to display the modified image
-                    inputImage.Image = (Bitmap)originalRadiograph.Clone();
                 }
+                resizedImage = ResizeImage(originalRadiograph, inputImage.Width, inputImage.Height);
+                inputImage.Image = resizedImage;
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private Color MapColor(int grayscaleValue)
         {
+            string colorMapType = comboColorMap.SelectedItem.ToString();
 
+            switch (colorMapType)
+            {
+                case "Hot":
+                    int red = grayscaleValue < 128 ? 0 : 255;
+                    int green = grayscaleValue < 128 ? grayscaleValue * 2 : 255;
+                    return Color.FromArgb(red, green, 255 - grayscaleValue);
+                case "Cool":
+                    int blue = grayscaleValue < 128 ? 255 : 0;
+                    int green1 = grayscaleValue < 128 ? 255 - grayscaleValue * 2 : 0;
+                    return Color.FromArgb(0, green1, blue);
+                case "Jet":
+                    if (grayscaleValue < 64)
+                        return Color.FromArgb(0, 0, (int)(4 * grayscaleValue));
+                    else if (grayscaleValue < 128)
+                        return Color.FromArgb(0, (int)(4 * (grayscaleValue - 64)), 255);
+                    else if (grayscaleValue < 192)
+                        return Color.FromArgb((int)(4 * (grayscaleValue - 128)), 255, (int)(255 - 4 * (grayscaleValue - 128)));
+                    else
+                        return Color.FromArgb(255, (int)(255 - 4 * (grayscaleValue - 192)), 0);
+                default:
+                    return Color.FromArgb(grayscaleValue, grayscaleValue, grayscaleValue);
+            }
+        }
+
+
+        private void comboColorMap_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (selectedArea.Width > 0 && selectedArea.Height > 0)
+            {
+                ApplyColorMapping();
+                inputImage.Invalidate();
+            }
+        }
+
+        private void saveImage_Click(object sender, EventArgs e)
+        {
+            if (originalRadiograph != null)
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "JPEG Image|*.jpg|PNG Image|*.png";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        ImageFormat format = ImageFormat.Jpeg;
+                        if (saveFileDialog.FileName.EndsWith(".png", StringComparison.OrdinalIgnoreCase))
+                        {
+                            format = ImageFormat.Png;
+                        }
+                        originalRadiograph.Save(saveFileDialog.FileName, format);
+                        MessageBox.Show("message saved successfully", "X-Ray", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
         }
     }
 }
